caf65d2bb9e6399cd8bef82772d6e8872ec1d630
jesus <jesusalbertoramirezlopez@gmail.com>
2021-06-22 18:34:35 -0500

    correccion a Objeto

--- cride/circles/views.py
+++ cride/circles/views.py
@@ -3,5 +3,5 @@ from cride.circles.models import Circle
 
 def list_circles(request):
     circles = Circle.objects.filter(is_public=True)
-    circles = [{x.name} for x in circles]
+    circles = [{'name':x.name} for x in circles]
     return JsonResponse(circles,safe=False)
