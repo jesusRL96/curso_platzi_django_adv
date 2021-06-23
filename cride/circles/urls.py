48d803fba978f22286b477f6ce6d762287e59720
jesus <jesus.alberto@desici.com>
2021-06-23 09:47:29 -0500

    Vistas API

--- cride/circles/urls.py
+++ cride/circles/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 
 from django.urls import path
 
-from cride.circles.views import list_circles
+from cride.circles.views import create_circle, list_circles
 
 urlpatterns = [
-    path('circles/', list_circles)
+    path('circles/', list_circles),
+    path('circles/create/', create_circle)
 ]
--- cride/circles/views.py
+++ cride/circles/views.py
@@ -1,7 +1,22 @@
 from django.http import HttpResponse, JsonResponse
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
 from cride.circles.models import Circle
 
+@api_view(["GET"])
 def list_circles(request):
-    circles = Circle.objects.filter(is_public=True)
+    circles = Circle.objects.all()
     circles = [{'name':x.name} for x in circles]
-    return JsonResponse(circles,safe=False)
+    return Response(circles)
+
+@api_view(["POST"])
+def create_circle(request):
+    """Create circle"""
+    name = request.data["name"]
+    about = request.data.get("about")
+    slug_name = request.data["slug_name"]
+    circle = Circle.objects.create(name=name, slug_name=slug_name, about=about)
+    data = {'name':name,'about':about, 'slug_name':slug_name}
+    return Response(data)
