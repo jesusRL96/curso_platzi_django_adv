d55fe82988734b4c1f6c463cf92f8c1e30370236
jesus <jesus.alberto@desici.com>
2021-06-23 11:54:42 -0500

    SignUp

--- cride/users/serializers.py
+++ cride/users/serializers.py
@@ -1,14 +1,49 @@
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, password_validation
+from django.core.validators import RegexValidator
 
 from rest_framework import serializers
 from rest_framework.authtoken.models import Token
+from rest_framework.validators import UniqueValidator
 
-from cride.users.models import User
+from cride.users.models import User, Profile
 
 class UserModelSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('username', 'email', 'first_name', 'last_name', 'phone_number')
+
+class UserSignUpSerializer(serializers.Serializer):
+    """User signup serializer"""
+    email = serializers.EmailField(validators=[UniqueValidator(User.objects.all())])
+    username = serializers.CharField(min_length=4, max_length=20, validators=[UniqueValidator(User.objects.all())])
+    # password
+    password = serializers.CharField(min_length=8, max_length=64)
+    password_confirmation = serializers.CharField(min_length=8, max_length=64)
+    #  phone number
+    phone_regex = RegexValidator(
+        regex=r'\+?1?\d{9,15}$',
+        message="Phone number must be entered in the format: +99999999. Up to 15 digits allowed."
+    )
+    phone_number = serializers.CharField(max_length=17, required=False, validators=[phone_regex,])
+    # Name
+    first_name = serializers.CharField(min_length=2, max_length=30)
+    last_name = serializers.CharField(min_length=2, max_length=30)
+
+    def validate(self, data):
+        """Check password match"""
+        password = data['password']
+        password_confirm = data['password_confirmation']
+        if password != password_confirm:
+            raise serializers.ValidationError('Password does not match')
+        password_validation.validate_password(password)
+        return data
+
+    def create(self, data):
+        data.pop('password_confirmation')
+        user = User.objects.create_user(**data)
+        profile = Profile.objects.create(user=user)
+        return user
+
 
 class UserLoginSerializer(serializers.Serializer):
     """User login serializer"""
--- cride/users/views.py
+++ cride/users/views.py
@@ -3,7 +3,7 @@ from rest_framework import status
 from rest_framework.response import Response
 
 
-from cride.users.serializers import UserLoginSerializer, UserModelSerializer
+from cride.users.serializers import UserLoginSerializer, UserModelSerializer, UserSignUpSerializer
 
 
 class UserLoginAPIView(APIView):
@@ -15,4 +15,12 @@ class UserLoginAPIView(APIView):
             'user': UserModelSerializer(user).data,
             'token': token
         }
+        return Response(data, status=status.HTTP_201_CREATED)
+
+class UserSignUpAPIView(APIView):
+    def post(self, request, *args, **kwargs):
+        serializer = UserSignUpSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        data = UserModelSerializer(user).data
         return Response(data, status=status.HTTP_201_CREATED)
