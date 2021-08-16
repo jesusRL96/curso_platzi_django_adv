f6232afda1a080b262d5071b385a549914b25cb7
jesus <jesus.alberto@desici.com>
2021-08-16 16:43:14 -0500

    DRF APITestCase

--- cride/circles/tests/test_invitations.py
+++ cride/circles/tests/test_invitations.py
@@ -1,7 +1,12 @@
+from cride.circles.models.memberships import Membership
 from django.test import TestCase
 
 from cride.circles.models import Invitation, Circle
-from cride.users.models import User
+from cride.users.models import User, Profile
+
+from rest_framework.test import APITestCase
+from rest_framework import status
+from rest_framework.authtoken import Token
 
 class InvitationsManagerTestCase(TestCase):
     def setUp(self):
@@ -33,3 +38,35 @@ class InvitationsManagerTestCase(TestCase):
         code = Invitation.objects.create(issued_by=self.user, circle=self.circle).code
         invitation = Invitation.objects.create(issued_by=self.user, circle=self.circle, code=code)
         self.assertNotEqual(code, invitation.code)
+class MemberInvitationsAPITestCase(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create(
+            first_name="Juan",
+            last_name="López",
+            email="juan.lopez@gmail.com",
+            username="juan1",
+            password="admin1",
+        )
+        self.profile = Profile.objects.create(user=self.user)
+        self.circle = Circle.objects.create(
+            name="circulo 1",
+            last_name="circle_1",
+            about="grupo circulo 1",
+            verified=True,
+        )
+        self.membership = Membership.objects.create(user=self.user, profile=self.profile, circle=self.circle)
+        self.token = Token.objects.create(user=self.user).key
+
+        self.client.credentials(HTTP_AUTHORIZATION=f'Token {self.token}')
+        self.url = f"/circles/{self.circle.slugname}/members/{self.user.username}/invitations/"
+
+    def test_response_success(self):
+        request = self.client.get(self.url)
+        self.assertEqual(request.status_code, status.HTTP_200_OK)
+
+    def self_invitation_creation(self):
+        self.assertEqual(Invitation.objects.all().count(), 0)
+
+        request = self.client.get(self.url)
+        self.assertEqual(request.status_code, status.HTTP_200_OK)
+        self.assertEqual(Invitation.objects.all().count(), 10)
