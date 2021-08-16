45155335aae28a92b2e94af3c0dd86fd9f52ab66
jesus <jesus.alberto@desici.com>
2021-08-16 16:47:59 -0500

    Admin actions: Modificar datos de un query

--- cride/circles/admin.py
+++ cride/circles/admin.py
@@ -8,6 +8,16 @@ class CircleAdmin(admin.ModelAdmin):
     search_fields = ('slug_name', 'name',)
     list_filter = ('verified', 'is_public', 'is_limited')
 
+    def make_verified(self, request, queryset):
+        queryset.update(verified=True)
+    make_verified.short_description = 'Make selected circles verified'
+
+    def make_unverified(self, request, queryset):
+        queryset.update(verified=False)
+    make_unverified.short_description = 'Make selected circles unverified'
+
+    actions = ['make_verified', 'make_unverified']
+
 @admin.register(Membership)
 class MembershipAdmin(admin.ModelAdmin):
     """Membership Admin"""
