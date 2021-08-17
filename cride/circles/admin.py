870596b1ba7285a25777e14c031c9026a5d0f754
jesus <jesus.alberto@desici.com>
2021-08-17 17:26:51 -0500

    Admin actions: Regresando una respuesta HTTP

--- cride/circles/admin.py
+++ cride/circles/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from cride.circles.models import Circle, Membership
+from django.utils import timezone
+from datetime import datetime, timedelta
+from django.http import HttpResponse
+
+from cride.rides.models import Ride
+import csv
 
 @admin.register(Circle)
 class CircleAdmin(admin.ModelAdmin):
@@ -16,7 +22,35 @@ class CircleAdmin(admin.ModelAdmin):
         queryset.update(verified=False)
     make_unverified.short_description = 'Make selected circles unverified'
 
-    actions = ['make_verified', 'make_unverified']
+    def download_todays_rides(self, request, queryset):
+        now = timezone.now()
+        start = datetime(now.year, now.month, now.day,0,0,0)
+        end = start + timedelta(days=1)
+        rides = Ride.objects.filter(offered_in__in=queryset.values_list('id'), departure_date__range=[start, end])
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filname="somefilename.csv"'
+        writer = csv.writer(response)
+        writer.writerow([
+            'id',
+            'passengers',
+            'departure_location',
+            'departure_date',
+            'arrival_location',
+            'arrival_date'
+            'rating'
+            ])
+        for ride in rides:
+            writer.writerow([
+                ride.pk,
+                ride.passengers.count(),
+                ride.departure_location,
+                str(ride.departure_date),
+                ride.arrival_location,
+                str(ride.arrival_date),
+                ride.rating
+                ])
+        return response
+    actions = ['make_verified', 'make_unverified', 'download_todays_rides']
 
 @admin.register(Membership)
 class MembershipAdmin(admin.ModelAdmin):
